84ae5a568018ce7373fa2887805bf568621d6766
Sergei <chertterner686@gmail.com>
2023-11-09 12:48:31 +0200

    first commit

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -2,10 +2,15 @@ package mate.academy.mapstruct.mapper;
 
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
+import mate.academy.mapstruct.mapperconfig.MapperConfiguration;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(config = MapperConfiguration.class)
 public interface GroupMapper {
     GroupDto toDto(Group group);
 
+    @Mapping(target = "id", ignore = true)
     Group toModel(CreateGroupRequestDto requestDto);
 }
--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -2,10 +2,15 @@ package mate.academy.mapstruct.mapper;
 
 import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
 import mate.academy.mapstruct.dto.subject.SubjectDto;
+import mate.academy.mapstruct.mapperconfig.MapperConfiguration;
 import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(config = MapperConfiguration.class)
 public interface SubjectMapper {
     SubjectDto toDto(Subject subject);
 
+    @Mapping(target = "id", ignore = true)
     Subject toModel(CreateSubjectRequestDto requestDto);
 }
