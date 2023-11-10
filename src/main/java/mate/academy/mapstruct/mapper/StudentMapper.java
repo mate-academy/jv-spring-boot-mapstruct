7a6cd9f2b7ec0b2988eb210771fdd12c5ecba1d2
Anastasiia Metelska <metelska.anastasiia@gmail.com>
2023-11-10 12:42:14 +0100

    mappers implemented using MapStruct

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,15 +1,59 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+@Mapper(config = MapperConfig.class, uses = GroupMapper.class)
 public interface StudentMapper {
+    @Mapping(
+            target = "subjectIds",
+            source = "subjects",
+            qualifiedByName = "mapSubjectsToLongs"
+    )
+    @Mapping(
+            target = "groupId",
+            source = "group.id"
+    )
     StudentDto toDto(Student student);
 
+    @Mapping(
+            target = "groupId",
+            source = "group.id"
+    )
     StudentWithoutSubjectsDto toEmployeeWithoutSubjectsDto(Student student);
 
-
+    @Mapping(
+            target = "subjects",
+            source = "requestDto.subjects",
+            qualifiedByName = "mapLongsToSubjects"
+    )
+    @Mapping(
+            target = "group",
+            source = "requestDto.groupId",
+            qualifiedByName = "groupById"
+    )
     Student toModel(CreateStudentRequestDto requestDto);
+
+    @Named("mapSubjectsToLongs")
+    default List<Long> mapSubjectsToLongs(List<Subject> subjects) {
+        return subjects.stream()
+                .map(Subject::getId)
+                .collect(Collectors.toList());
+    }
+
+    @Named("mapLongsToSubjects")
+    default List<Subject> mapLongsToSubjects(List<Long> subjectIds) {
+        return subjectIds.stream()
+                .map(Subject::new)
+                .collect(Collectors.toList());
+    }
 }
