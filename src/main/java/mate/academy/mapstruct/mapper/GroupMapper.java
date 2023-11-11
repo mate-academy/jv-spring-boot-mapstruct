f6b30bea5ebbf3c5b72274a0ce9aa64076b13344
Yurii_Ivanov <128720591+IvanovYurii@users.noreply.github.com>
2023-11-11 03:41:16 +0200

    Solution of this task

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -1,11 +1,27 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.Optional;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.Named;
 
+@Mapper(componentModel = "spring")
 public interface GroupMapper {
     GroupDto toDto(Group group);
 
     Group toModel(CreateGroupRequestDto requestDto);
+
+    @Named("groupById")
+    default Group groupById(Long id) {
+        return Optional.ofNullable(id)
+                .map(Group::new)
+                .orElse(null);
+    }
+
+    @Named("idByGroup")
+    default Long idByGroup(Group group) {
+        return group.getId();
+    }
 }
