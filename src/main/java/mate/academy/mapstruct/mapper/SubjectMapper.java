8c2630f506d9ef772ac8a07e432bb593ad533e79
vasyl.moshun <vasyl.moshun@gmail.com>
2023-11-14 17:07:34 +0200

    fix

--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -1,9 +1,5 @@
 package mate.academy.mapstruct.mapper;
 
-import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
-import mate.academy.mapstruct.dto.subject.SubjectDto;
-import mate.academy.mapstruct.model.Subject;
-
 import java.util.List;
 import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
