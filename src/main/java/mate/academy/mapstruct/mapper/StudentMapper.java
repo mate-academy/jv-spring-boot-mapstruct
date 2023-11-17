e37a1b141b4dd952eb33193c50c35237b81ea596
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-11-17 21:38:57 +0200

    all tests passed

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,15 +1,48 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.List;
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
+import mate.academy.mapstruct.model.Group;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
+@Mapper(config = MapperConfig.class)
 public interface StudentMapper {
+    @Mapping(target = "groupId", source = "group.id")
     StudentDto toDto(Student student);
 
-    StudentWithoutSubjectsDto toEmployeeWithoutSubjectsDto(Student student);
+    @AfterMapping
+    default void setSubjectIds(@MappingTarget StudentDto studentDto, Student student) {
+        List<Long> subjectIds = student.getSubjects().stream()
+                .map(Subject::getId)
+                .toList();
+        studentDto.setSubjectIds(subjectIds);
+    }
 
+    @Mapping(target = "groupId", source = "group.id")
+    StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
     Student toModel(CreateStudentRequestDto requestDto);
+
+    @AfterMapping
+    default void setGroup(@MappingTarget Student student, CreateStudentRequestDto requestDto) {
+        Group group = new Group();
+        group.setId(requestDto.groupId());
+        student.setGroup(group);
+    }
+
+    @AfterMapping
+    default void setSubjects(@MappingTarget Student student, CreateStudentRequestDto requestDto) {
+        List<Subject> subjects = requestDto.subjectIds().stream()
+                .map(Subject::new)
+                .toList();
+        student.setSubjects(subjects);
+    }
 }
