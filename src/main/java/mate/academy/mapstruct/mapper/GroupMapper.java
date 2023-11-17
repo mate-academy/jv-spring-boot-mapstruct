5bbc9469522b44934efd4e2ba3c628ad7ae718d0
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-11-17 19:46:48 +0200

    implemented mappers

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -1,11 +1,27 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.Optional;
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+@Mapper(config = MapperConfig.class)
 public interface GroupMapper {
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
     GroupDto toDto(Group group);
 
+    @Mapping(source = "name", target = "name")
     Group toModel(CreateGroupRequestDto requestDto);
+
+    @Named("getGroupById")
+    default Group getGroupById(Long id) {
+        return Optional.ofNullable(id)
+                .map(Group::new)
+                .orElse(null);
+    }
 }
--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -1,11 +1,18 @@
 package mate.academy.mapstruct.mapper;
 
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
 import mate.academy.mapstruct.dto.subject.SubjectDto;
 import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(config = MapperConfig.class)
 public interface SubjectMapper {
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
     SubjectDto toDto(Subject subject);
 
+    @Mapping(source = "name", target = "name")
     Subject toModel(CreateSubjectRequestDto requestDto);
 }
