7f6727c761a372517fbf2a976f7db63a9feee1e8
Igor Korolevich <igorkorolj@gmail.com>
2023-12-08 15:32:49 +0200

    done!

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,15 +1,33 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.List;
+import mate.academy.mapstruct.dto.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.springframework.stereotype.Component;
 
+@Component
+@Mapper(config = MapperConfig.class, uses = {SubjectMapper.class, GroupMapper.class})
 public interface StudentMapper {
+    @Mapping(target = "groupId", source = "group", qualifiedByName = "getIdFromGroup")
+    @Mapping(target = "subjectIds", source = "subjects", qualifiedByName = "getIdsFromSubjects")
     StudentDto toDto(Student student);
 
+    @Mapping(source = "group", target = "groupId", qualifiedByName = "getIdFromGroup")
     StudentWithoutSubjectsDto toEmployeeWithoutSubjectsDto(Student student);
 
-
+    @Mapping(target = "group", source = "groupId", qualifiedByName = "groupById")
+    @Mapping(target = "subjects", source = "subjects", qualifiedByName = "getSubjectsByIds")
     Student toModel(CreateStudentRequestDto requestDto);
+
+    default void setSubject(@MappingTarget Student student, CreateStudentRequestDto requestDto) {
+        List<Subject> subjects = requestDto.subjects().stream().map(Subject::new).toList();
+        student.setSubjects(subjects);
+    }
 }
--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -1,11 +1,28 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.List;
+import mate.academy.mapstruct.dto.config.MapperConfig;
 import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
 import mate.academy.mapstruct.dto.subject.SubjectDto;
 import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.Named;
+import org.springframework.stereotype.Component;
 
+@Component
+@Mapper(config = MapperConfig.class)
 public interface SubjectMapper {
     SubjectDto toDto(Subject subject);
 
     Subject toModel(CreateSubjectRequestDto requestDto);
+
+    @Named("getSubjectsByIds")
+    default List<Subject> getSubjectsByIds(List<Long> longList) {
+        return longList.stream().map(Subject::new).toList();
+    }
+
+    @Named("getIdsFromSubjects")
+    default List<Long> getIdsFromSubjects(List<Subject> subjects) {
+        return subjects.stream().map(subject -> subject.getId()).toList();
+    }
 }
