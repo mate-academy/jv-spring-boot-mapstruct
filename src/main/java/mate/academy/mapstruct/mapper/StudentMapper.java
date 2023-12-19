44434d773deafbb11444b5bf2eee42a91aacf2e0
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-12-19 19:04:55 +0200

    implemented task

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -4,12 +4,22 @@ import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(componentModel = "spring", uses = {GroupMapper.class, SubjectMapper.class})
 public interface StudentMapper {
+
+    @Mapping(target = "groupId", source = "group", qualifiedByName = "getGroupId")
+    @Mapping(target = "subjectIds", source = "subjects", qualifiedByName = "getSubjectIds")
     StudentDto toDto(Student student);
 
+    @Mapping(target = "groupId", source = "group", qualifiedByName = "getGroupId")
     StudentWithoutSubjectsDto toEmployeeWithoutSubjectsDto(Student student);
 
-
+    @Mapping(target = "group", source = "groupId", qualifiedByName = "getGroupById")
+    @Mapping(target = "subjects", source = "subjects", qualifiedByName = "getSubjectsByIds")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "socialSecurityNumber", ignore = true)
     Student toModel(CreateStudentRequestDto requestDto);
 }
