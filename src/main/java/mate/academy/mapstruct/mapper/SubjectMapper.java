164061e53f84179de0db8b2cfbdf85b0a407bb83
Nikita <kaptetos@gmail.com>
2023-12-21 16:01:55 +0200

    fulfilled according to the conditions

--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -1,11 +1,33 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
 import mate.academy.mapstruct.dto.subject.SubjectDto;
 import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+@Mapper(componentModel = "spring")
 public interface SubjectMapper {
     SubjectDto toDto(Subject subject);
 
+    @Mapping(target = "id", ignore = true)
     Subject toModel(CreateSubjectRequestDto requestDto);
+
+    @Named("getSubjectId")
+    default Long getSubjectId(Subject subject) {
+        return subject.getId();
+    }
+
+    @Named("getSubjectIds")
+    default List<Long> getSubjectIds(List<Subject> subjects) {
+        return subjects.stream().map(Subject::getId).collect(Collectors.toList());
+    }
+
+    @Named("getSubjectsByIds")
+    default List<Subject> getSubjectsByIds(List<Long> subjects) {
+        return subjects.stream().map(Subject::new).collect(Collectors.toList());
+    }
 }
