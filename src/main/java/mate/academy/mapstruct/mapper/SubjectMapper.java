d31846150ba4717a5ce0681cecf40876cc9e8f67
Jakub Sekowski <sekowskijakub@gmail.com>
2024-02-03 14:35:14 +0100

    First solution of jv-spring-boot-mapstruct

--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -3,7 +3,10 @@ package mate.academy.mapstruct.mapper;
 import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
 import mate.academy.mapstruct.dto.subject.SubjectDto;
 import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
 
+@Mapper(config = MapperConfig.class)
 public interface SubjectMapper {
     SubjectDto toDto(Subject subject);
 
