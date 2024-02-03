bbc4ca06d1550de04b30445d7150d0f1fc09c3f8
Jakub Sekowski <sekowskijakub@gmail.com>
2024-02-03 15:11:46 +0100

    Fixes applied

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -1,23 +1,27 @@
 package mate.academy.mapstruct.mapper;
 
-import java.util.Optional;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
 import org.mapstruct.Mapper;
 import org.mapstruct.MapperConfig;
+import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 @Mapper(config = MapperConfig.class)
 public interface GroupMapper {
     GroupDto toDto(Group group);
 
+    @Mapping(target = "id", ignore = true)
     Group toModel(CreateGroupRequestDto requestDto);
 
-    @Named("groupById")
-    default Group groupById(Long id) {
-        return Optional.ofNullable(id)
-                .map(Group::new)
-                .orElse(null);
+    @Named("getGroupId")
+    default Long getGroupId(Group group) {
+        return group.getId();
+    }
+
+    @Named("getGroupById")
+    default Group getGroupById(Long id) {
+        return new Group(id);
     }
 }
--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,21 +1,26 @@
 package mate.academy.mapstruct.mapper;
 
-import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
 import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
 import org.mapstruct.Mapping;
 
-@Mapper(config = MapperConfig.class, uses = GroupMapper.class)
+@Mapper(config = MapperConfig.class, uses = {GroupMapper.class, SubjectMapper.class})
 public interface StudentMapper {
+
+    @Mapping(target = "groupId", source = "group", qualifiedByName = "getGroupId")
+    @Mapping(target = "subjectIds", source = "subjects", qualifiedByName = "getSubjectIds")
     StudentDto toDto(Student student);
 
-    @Mapping(source = "group.id", target = "groupId")
+    @Mapping(target = "groupId", source = "group", qualifiedByName = "getGroupId")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
-    @Mapping(target = "group", source = "groupId", qualifiedByName = "groupById")
-    @Mapping(target = "subjects", ignore = true)
+    @Mapping(target = "group", source = "groupId", qualifiedByName = "getGroupById")
+    @Mapping(target = "subjects", source = "subjects", qualifiedByName = "getSubjectsByIds")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "socialSecurityNumber", ignore = true)
     Student toModel(CreateStudentRequestDto requestDto);
 }
