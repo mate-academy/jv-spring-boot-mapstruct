572a09c05ea2665bcd23f2d4d3e66bda463d183b
Dmytro Senko <@@dmytro.senko>
2024-02-23 12:57:42 +0200

    feat: add mapping

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -1,11 +1,24 @@
 package mate.academy.mapstruct.mapper;
 
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.Named;
 
+@Mapper(config = MapperConfig.class)
 public interface GroupMapper {
     GroupDto toDto(Group group);
 
     Group toModel(CreateGroupRequestDto requestDto);
+
+    @Named("getGroupById")
+    default Group getGroupById(Long id) {
+        if (id == null) {
+            return null;
+
+        }
+        return new Group(id);
+    }
 }
