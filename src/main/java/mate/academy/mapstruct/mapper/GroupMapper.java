25f618bf035f790a1bdd4c1c1d72a27446f58445
artem <artemsa223@gmail.com>
2024-02-26 20:34:19 +0000

    implementation mappers

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -1,5 +1,6 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.Optional;
 import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
@@ -7,13 +8,12 @@ import mate.academy.mapstruct.model.Group;
 import org.mapstruct.Mapper;
 import org.mapstruct.Named;
 
-import java.util.Optional;
-
 @Mapper (config = MapperConfig.class)
 public interface GroupMapper {
     GroupDto toDto(Group group);
 
     Group toModel(CreateGroupRequestDto requestDto);
+
     @Named("groupById")
     default Group groupById(Long id) {
         return Optional.ofNullable(id)
--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,5 +1,6 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.List;
 import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
@@ -11,14 +12,19 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-import java.util.List;
-
 @Mapper(config = MapperConfig.class, uses = GroupMapper.class)
 public interface StudentMapper {
 
     @Mapping(source = "group.id", target = "groupId")
     @Mapping(target = "subjectIds", ignore = true)
     StudentDto toDto(Student student);
+
+    @Mapping(target = "groupId", source = "group.id")
+    StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
+
+    @Mapping(target = "group", source = "groupId", qualifiedByName = "groupById")
+    @Mapping(target = "subjects", ignore = true)
+    Student toModel(CreateStudentRequestDto requestDto);
 
     @AfterMapping
     default void setSubjectIds(@MappingTarget StudentDto studentDto, Student student) {
@@ -27,12 +33,6 @@ public interface StudentMapper {
                 .toList();
         studentDto.setSubjectIds(subjectIds);
     }
-    @Mapping( target = "groupId", source ="group.id")
-    StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
-    @Mapping( target = "group", source = "groupId", qualifiedByName = "groupById")
-    @Mapping(target = "subjects", ignore = true)
-    Student toModel(CreateStudentRequestDto requestDto);
-
 
     @AfterMapping
     default void setSubjectIds(@MappingTarget Student student, CreateStudentRequestDto requestDto) {
