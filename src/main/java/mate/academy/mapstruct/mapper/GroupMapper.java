e2143c79f01b18bdd1b90273c074ca1824f711ca
Mykola Voronov <www.kolyavoronov@gmail.com>
2024-03-28 20:13:33 +0200

    Mappers refactoring using mapstruct

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -3,9 +3,17 @@ package mate.academy.mapstruct.mapper;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.ReportingPolicy;
 
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface GroupMapper {
     GroupDto toDto(Group group);
 
+    @Mappings({
+            @Mapping(target = "id", ignore = true)
+    })
     Group toModel(CreateGroupRequestDto requestDto);
 }
