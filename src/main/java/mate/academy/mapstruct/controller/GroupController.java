86ba2ec671662f6c46bf58cff92d25a236a41d69
VolandevlodD <vladradko2000@gmail.com>
2024-04-23 17:53:29 +0300

    MapStruct Mappers Implementation

--- src/main/java/mate/academy/mapstruct/controller/GroupController.java
+++ src/main/java/mate/academy/mapstruct/controller/GroupController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/groups")
+    @RequestMapping(value = "/groups")
 public class GroupController {
 
     private final GroupService groupService;
