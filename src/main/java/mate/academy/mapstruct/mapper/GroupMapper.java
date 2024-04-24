8bb29efecb030c5495a054be54fed93e8e59ad1a
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-04-24 21:53:48 +0300

    add main version

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -1,11 +1,16 @@
 package mate.academy.mapstruct.mapper;
 
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(config = MapperConfig.class)
 public interface GroupMapper {
     GroupDto toDto(Group group);
 
+    @Mapping(target = "id", ignore = true)
     Group toModel(CreateGroupRequestDto requestDto);
 }
