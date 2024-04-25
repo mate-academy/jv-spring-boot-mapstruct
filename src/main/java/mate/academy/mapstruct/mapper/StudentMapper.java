db4b75fc5bf0708587720c1fe1ade7fdbf1012c7
Bohdan Markatov <bogdanmarkatov@gmail.com>
2024-04-26 00:07:58 +0300

    config all mappers

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,14 +1,23 @@
 package mate.academy.mapstruct.mapper;
 
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(config = MapperConfig.class, uses = {GroupMapper.class, SubjectMapper.class})
 public interface StudentMapper {
+    @Mapping(target = "groupId", source = "group", qualifiedByName = "getGroupId")
+    @Mapping(target = "subjectIds", source = "subjects", qualifiedByName = "getSubjectsId")
     StudentDto toDto(Student student);
 
+    @Mapping(target = "groupId", source = "group", qualifiedByName = "getGroupId")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
+    @Mapping(target = "group", source = "groupId", qualifiedByName = "groupById")
+    @Mapping(target = "subjects", source = "subjects", qualifiedByName = "toSubjects")
     Student toModel(CreateStudentRequestDto requestDto);
 }
