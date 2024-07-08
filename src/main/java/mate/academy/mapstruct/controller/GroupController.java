aff97924a6209b7a2a318994d48e1a75c8b7a88f
Vadym <Servicepost.v@gmail.com>
2024-07-08 22:23:59 +0200

    .

--- src/main/java/mate/academy/mapstruct/controller/GroupController.java
+++ src/main/java/mate/academy/mapstruct/controller/GroupController.java
@@ -1,23 +1,30 @@
-package mate.academy.mapstruct.mapper;
+package mate.academy.mapstruct.controller;
 
-import java.util.Optional;
-import mate.academy.mapstruct.config.MapperConfig;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
-import mate.academy.mapstruct.model.Group;
-import org.mapstruct.Mapper;
-import org.mapstruct.Named;
+import mate.academy.mapstruct.service.group.GroupService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Mapper(config = MapperConfig.class)
-public interface GroupMapper {
-    GroupDto toDto(Group group);
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/groups")
+public class GroupController {
 
-    Group toModel(CreateGroupRequestDto requestDto);
+    private final GroupService groupService;
 
-    @Named("groupGetById")
-    default Group groupGetById(Long id) {
-        return Optional.ofNullable(id)
-                .map(Group::new)
-                .orElse(null);
+    @GetMapping
+    public List<GroupDto> findAll() {
+        return groupService.findAll();
+    }
+
+    @PostMapping
+    public GroupDto save(@RequestBody CreateGroupRequestDto requestDto) {
+        return groupService.save(requestDto);
     }
 }
