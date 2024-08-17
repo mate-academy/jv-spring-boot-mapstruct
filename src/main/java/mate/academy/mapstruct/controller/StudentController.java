0108291e0607cbc75a2d36b54dafc7d8aba26e14
Mykhailo Levko <mykhailo.levko@gmail.com>
2024-08-17 14:39:41 -0400

    Implemented mappers with using the MapStruct library

--- src/main/java/mate/academy/mapstruct/controller/StudentController.java
+++ src/main/java/mate/academy/mapstruct/controller/StudentController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/students")
 public class StudentController {
-
     private final StudentService studentService;
 
     @GetMapping
---
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapperImpl.java
@@ -0,0 +1,59 @@
+package mate.academy.mapstruct.mapper;
+
+import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
+import mate.academy.mapstruct.dto.student.StudentDto;
+import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
+import mate.academy.mapstruct.model.Student;
+
+public class StudentMapperImpl implements StudentMapper {
+    private final GroupMapper groupMapper;
+
+    public StudentMapperImpl(GroupMapper groupMapper) {
+        this.groupMapper = groupMapper;
+    }
+
+    @Override
+    public StudentDto toDto(Student student) {
+        if (student == null) {
+            return null;
+        }
+
+        StudentDto studentDto = new StudentDto();
+        studentDto.setId(student.getId());
+        studentDto.setName(student.getName());
+        studentDto.setEmail(student.getEmail());
+        if (student.getGroup() != null) {
+            studentDto.setGroupId(student.getGroup().getId());
+        }
+        setSubjectsIds(studentDto, student);
+        return studentDto;
+    }
+
+    @Override
+    public StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student) {
+        if (student == null) {
+            return null;
+        }
+        StudentWithoutSubjectsDto studentDto = new StudentWithoutSubjectsDto();
+        studentDto.setId(student.getId());
+        studentDto.setName(student.getName());
+        studentDto.setEmail(student.getEmail());
+        if (student.getGroup() != null) {
+            studentDto.setGroupId(student.getGroup().getId());
+        }
+        return studentDto;
+    }
+
+    @Override
+    public Student toModel(CreateStudentRequestDto requestDto) {
+        if (requestDto == null) {
+            return null;
+        }
+        Student student = new Student();
+        student.setName(requestDto.name());
+        student.setEmail(requestDto.email());
+        student.setGroup(groupMapper.groupById(requestDto.groupId()));
+        setSubjects(student, requestDto);
+        return student;
+    }
+}
