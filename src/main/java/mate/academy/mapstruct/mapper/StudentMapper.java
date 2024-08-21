5c4dca854894ce987d7e49190b061198c8bb4d20
Andrii Poluhovic <andriy.poluhovic10@gmail.com>
2024-08-21 15:40:12 +0200

    Implemented mapstruct logic

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -4,11 +4,20 @@ import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(componentModel = "spring", uses = {SubjectMapper.class, GroupMapper.class})
 public interface StudentMapper {
+    @Mapping(target = "groupId", source = "student.group.id")
+    @Mapping(target = "subjectIds", source = "subjects")
     StudentDto toDto(Student student);
 
+    @Mapping(target = "groupId", source = "student.group.id")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "socialSecurityNumber", ignore = true)
+    @Mapping(target = "group", source = "groupId")
     Student toModel(CreateStudentRequestDto requestDto);
 }
