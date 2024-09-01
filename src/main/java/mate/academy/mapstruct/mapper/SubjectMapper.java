6605d90d194f10a22bcfb37227b4a3be2520bbac
Jakub <jszymanski97@gmail.com>
2024-09-01 18:05:25 +0100

    First attempt

--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -1,11 +1,37 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
 import mate.academy.mapstruct.dto.subject.SubjectDto;
 import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+@Mapper(config = MapperConfig.class)
 public interface SubjectMapper {
     SubjectDto toDto(Subject subject);
 
+    @Mapping(target = "id", ignore = true)
     Subject toModel(CreateSubjectRequestDto requestDto);
+
+    @Named("subjectsById")
+    default List<Long> subjectsById(List<Subject> subjects) {
+        return subjects == null ? null : subjects.stream()
+                .map(Subject::getId)
+                .collect(Collectors.toList());
+    }
+
+    @Named("subjectIdToSubject")
+    default List<Subject> subjectIdToSubject(List<Long> subjects) {
+        return subjects == null ? null : subjects.stream()
+                .map(id -> {
+                    Subject subject = new Subject();
+                    subject.setId(id);
+                    return subject;
+                })
+                .collect(Collectors.toList());
+    }
 }
