97a2da84d4daa8b4b8d395e46e94530eb2579d79
Sasha Fetisov <sasafetisov03@gmail.com>
2024-09-11 14:27:10 +0300

    Commit for test

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -3,9 +3,23 @@ package mate.academy.mapstruct.mapper;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.Named;
+import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
+@Mapper(config = MapperConfig.class)
 public interface GroupMapper {
     GroupDto toDto(Group group);
 
     Group toModel(CreateGroupRequestDto requestDto);
+
+    @Named("groupById")
+    default Group groupById(Long id) {
+        return Optional.ofNullable(id)
+                .map(Group::new)
+                .orElse(null);
+    }
 }
