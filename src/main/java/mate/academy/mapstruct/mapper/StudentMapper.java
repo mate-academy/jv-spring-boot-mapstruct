281cb79409652cb1bb6d8f05df90a088b069b10a
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-12 07:45:41 +0300

    StudentMapper.java

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -4,13 +4,18 @@ import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface StudentMapper {
     @Mapping(source = "group.id", target = "groupId")
-    @Mapping(source = "subjects.id", target = "subjectIds")
+    @Mapping(source = "subjects", target = "subjectIds", qualifiedByName = "mapSubjectsToIds")
     StudentDto toDto(Student student);
 
     @Mapping(source = "group.id", target = "groupId")
@@ -19,4 +24,11 @@ public interface StudentMapper {
     @Mapping(target = "group.id", source = "groupId")
     @Mapping(target = "subjects", ignore = true) // Assuming subjects will be set separately
     Student toModel(CreateStudentRequestDto requestDto);
+
+    @Named("mapSubjectsToIds")
+    default List<Long> mapSubjectsToIds(List<Subject> subjects) {
+        return subjects.stream()
+                .map(Subject::getId)
+                .collect(Collectors.toList());
+    }
 }
