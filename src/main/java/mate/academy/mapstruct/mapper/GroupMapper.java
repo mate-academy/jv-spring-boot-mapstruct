17bf2de6f72981885015a58b6efbfdc974f6bfcc
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-12 07:45:55 +0300

    GroupMapper.java

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -4,10 +4,13 @@ import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface GroupMapper {
+    @Mapping(target = "id", source = "id")
     GroupDto toDto(Group group);
 
+    @Mapping(target = "id", ignore = true)
     Group toModel(CreateGroupRequestDto requestDto);
 }
