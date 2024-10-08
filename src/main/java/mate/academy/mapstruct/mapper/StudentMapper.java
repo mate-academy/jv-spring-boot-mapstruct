67ba74de5652d456fabe979f3244dd340b358f2f
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2024-10-08 15:21:10 +0200

    done all!

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,14 +1,43 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.List;
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
+@Mapper(config = MapperConfig.class, uses = GroupMapper.class)
 public interface StudentMapper {
+    @Mapping(target = "groupId", source = "group.id")
+    @Mapping(target = "subjectIds", ignore = true)
     StudentDto toDto(Student student);
 
+    @AfterMapping
+    default void setSubjectIds(@MappingTarget StudentDto dto, Student student) {
+        List<Long> ids = student.getSubjects().stream()
+                .map(Subject::getId)
+                .toList();
+        dto.setSubjectIds(ids);
+    }
+
+    @Mapping(source = "group.id", target = "groupId")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
+    @Mapping(target = "group", source = "groupId", qualifiedByName = "groupById")
+    @Mapping(target = "subjects", ignore = true)
     Student toModel(CreateStudentRequestDto requestDto);
+
+    @AfterMapping
+    default void setSubjects(@MappingTarget Student student, CreateStudentRequestDto dto) {
+        List<Subject> subjects = dto.subjects().stream()
+                .map(Subject::new)
+                .toList();
+        student.setSubjects(subjects);
+    }
 }
