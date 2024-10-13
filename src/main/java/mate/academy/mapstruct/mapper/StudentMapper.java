78b140b1699fa08bc0f9934951ea5a91add4dadb
Anton <StarAntonU@gmail.com>
2024-10-13 15:37:24 +0300

    impl mapper for group in student

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,14 +1,19 @@
 package mate.academy.mapstruct.mapper;
 
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(config = MapperConfig.class, uses = GroupMapper.class)
 public interface StudentMapper {
     StudentDto toDto(Student student);
 
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
+    @Mapping(target = "group", source = "groupId", qualifiedByName = "groupById")
     Student toModel(CreateStudentRequestDto requestDto);
 }
