777018c6ef526cd40ad64e0e9c41a69413f22908
Deschna <pashkadit@gmail.com>
2024-11-05 15:24:41 +0200

    task complete

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -1,9 +1,12 @@
 package mate.academy.mapstruct.mapper;
 
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
 
+@Mapper(config = MapperConfig.class)
 public interface GroupMapper {
     GroupDto toDto(Group group);
 
