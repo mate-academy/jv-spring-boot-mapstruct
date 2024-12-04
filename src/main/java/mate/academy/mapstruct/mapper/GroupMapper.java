e006ed6d92047a06fa038a13e7b90ea7da35875a
Ihor <ihor.rome@gmail.com>
2024-12-04 20:45:46 +0200

    add solution

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -1,11 +1,25 @@
 package mate.academy.mapstruct.mapper;
 
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.Named;
 
+@Mapper(config = MapperConfig.class)
 public interface GroupMapper {
     GroupDto toDto(Group group);
 
     Group toModel(CreateGroupRequestDto requestDto);
+
+    @Named("setGroupId")
+    default Long setGroupId(Group group) {
+        return group.getId();
+    }
+
+    @Named("setGroup")
+    default Group setGroup(Long groupId) {
+        return new Group(groupId);
+    }
 }
--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,14 +1,41 @@
 package mate.academy.mapstruct.mapper;
 
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
+@Mapper(config = MapperConfig.class, uses = {GroupMapper.class})
 public interface StudentMapper {
+    @Mapping(target = "groupId", source = "group", qualifiedByName = "setGroupId")
+    @Mapping(target = "subjectIds", ignore = true)
     StudentDto toDto(Student student);
 
+    @AfterMapping
+    default void setSubjectIds(@MappingTarget StudentDto studentDto, Student student) {
+        studentDto.setSubjectIds(student.getSubjects().stream()
+                .map(Subject::getId)
+                .toList());
+    }
+
+    @Mapping(target = "groupId", source = "group", qualifiedByName = "setGroupId")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
+    @Mapping(target = "group", source = "groupId", qualifiedByName = "setGroup")
+    @Mapping(target = "subjects", ignore = true)
     Student toModel(CreateStudentRequestDto requestDto);
+
+    @AfterMapping
+    default void setSubjects(@MappingTarget Student student,
+                                      CreateStudentRequestDto requestDto) {
+        student.setSubjects(requestDto.subjects().stream()
+                .map(Subject::new)
+                .toList());
+    }
 }
