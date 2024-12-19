ce652cfa4a37c950cdeaad5aac639e271f2af180
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-12-19 17:12:21 +0200

    Fix

--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -1,13 +1,12 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.Optional;
 import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
 import mate.academy.mapstruct.dto.subject.SubjectDto;
 import mate.academy.mapstruct.model.Subject;
 import org.mapstruct.Mapper;
 import org.mapstruct.Named;
-
-import java.util.Optional;
 
 @Mapper(config = MapperConfig.class)
 public interface SubjectMapper {
