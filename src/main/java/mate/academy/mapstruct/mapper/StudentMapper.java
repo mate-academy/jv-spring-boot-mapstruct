2908b9e88d853aeb57b28dc1e034032a4f4b702b
B. Smreczak <b.smreczak@gmail.com>
2024-12-25 12:05:41 +0100

    First solution, configured and used MapStruct for mapping objects.

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,14 +1,29 @@
 package mate.academy.mapstruct.mapper;
 
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
+@Mapper(config = MapperConfig.class, uses = {GroupMapper.class, SubjectMapper.class})
 public interface StudentMapper {
+
+    @Mappings({
+            @Mapping(source = "group.id", target = "groupId"),
+            @Mapping(source = "subjects", target = "subjectIds", qualifiedByName = "subjectsToIds")
+    })
     StudentDto toDto(Student student);
 
+    @Mapping(source = "group.id", target = "groupId")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
+    @Mappings({
+            @Mapping(source = "groupId", target = "group", qualifiedByName = "groupById"),
+            @Mapping(source = "subjects", target = "subjects", qualifiedByName = "idsToSubjects")
+    })
     Student toModel(CreateStudentRequestDto requestDto);
 }
