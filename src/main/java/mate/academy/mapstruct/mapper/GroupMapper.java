d8f34eaa426dbf162d510c47eda2b75fed7d6129
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-01-16 19:15:06 +0200

    GroupMapper.java

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -4,9 +4,13 @@ import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface GroupMapper {
+
+    @Mapping(target = "id", ignore = true) // If you want to ignore the ID during creation
     GroupDto toDto(Group group);
+
     Group toModel(CreateGroupRequestDto requestDto);
 }
