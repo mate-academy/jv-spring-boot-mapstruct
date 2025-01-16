33ac8250244647b73c7d3ea262b84d59188de5f1
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-01-16 19:14:49 +0200

    SubjectMapper.java

--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -4,9 +4,13 @@ import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
 import mate.academy.mapstruct.dto.subject.SubjectDto;
 import mate.academy.mapstruct.model.Subject;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SubjectMapper {
+
+    @Mapping(target = "id", ignore = true) // If you want to ignore the ID during creation
     SubjectDto toDto(Subject subject);
+
     Subject toModel(CreateSubjectRequestDto requestDto);
 }
