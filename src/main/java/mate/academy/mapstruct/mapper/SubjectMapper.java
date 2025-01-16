6dc388c612273bab0e5a6a716fac433fa958051d
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-01-16 19:11:50 +0200

    SubjectMapper.java

--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -3,9 +3,10 @@ package mate.academy.mapstruct.mapper;
 import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
 import mate.academy.mapstruct.dto.subject.SubjectDto;
 import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface SubjectMapper {
     SubjectDto toDto(Subject subject);
-
     Subject toModel(CreateSubjectRequestDto requestDto);
 }
