8acad0a42a1971607456c07beb9aeb3ad334c7b0
Ihor Tkachuk <iono19031@gmail.com>
2025-01-16 13:06:17 +0200

    added solution

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -3,9 +3,14 @@ package mate.academy.mapstruct.mapper;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(componentModel = "spring")
 public interface GroupMapper {
+    @Mapping(source = "id", target = "id")
     GroupDto toDto(Group group);
 
+    @Mapping(source = "name", target = "name")
     Group toModel(CreateGroupRequestDto requestDto);
 }
--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,14 +1,54 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
+import mate.academy.mapstruct.model.Group;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+@Mapper(componentModel = "spring", uses = {GroupMapper.class, SubjectMapper.class})
 public interface StudentMapper {
+    @Mapping(target = "groupId", source = "group.id")
+    @Mapping(target = "subjectIds", source = "subjects", qualifiedByName = "mapSubjects")
     StudentDto toDto(Student student);
 
+    @Mapping(target = "groupId", source = "group.id")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
+    @Mapping(target = "group", source = "groupId", qualifiedByName = "groupById")
+    @Mapping(target = "subjects", source = "subjectIds", qualifiedByName = "mapSubjectIds")
     Student toModel(CreateStudentRequestDto requestDto);
+
+    @Named("groupById")
+    default Group groupById(Long groupId) {
+        if (groupId == null) {
+            return null;
+        }
+        Group group = new Group();
+        group.setId(groupId);
+        return group;
+    }
+
+    @Named("mapSubjects")
+    default List<Long> mapSubjects(List<Subject> subjects) {
+        return subjects.stream()
+                .map(Subject::getId)
+                .collect(Collectors.toList());
+    }
+
+    @Named("mapSubjectIds")
+    default List<Subject> mapSubjectIds(List<Long> subjectIds) {
+        return subjectIds.stream()
+                .map(id -> {
+                    Subject subject = new Subject();
+                    subject.setId(id);
+                    return subject;
+                }).collect(Collectors.toList());
+    }
 }
