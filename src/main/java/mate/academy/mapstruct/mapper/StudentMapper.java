7776371369f7f56c6632986b2fee4f590c3ca7f6
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-01-16 19:11:31 +0200

    StudentMapper.java

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -4,11 +4,11 @@ import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface StudentMapper {
     StudentDto toDto(Student student);
-
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
-
     Student toModel(CreateStudentRequestDto requestDto);
 }
