7ba72ecfe70ef63ac1964cb663adb270a4e1827f
Oleh B <oleg.busygin@gmail.com>
2025-01-29 13:41:29 +0200

    done

--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -1,11 +1,26 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.Optional;
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
 import mate.academy.mapstruct.dto.subject.SubjectDto;
 import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+@Mapper(config = MapperConfig.class)
 public interface SubjectMapper {
+
+    @Mapping(target = "id", source = "subject.id")
     SubjectDto toDto(Subject subject);
 
     Subject toModel(CreateSubjectRequestDto requestDto);
+
+    @Named("getSubjectById")
+    default Subject getSubjectById(Long id) {
+        return Optional.ofNullable(id)
+                .map(Subject::new)
+                .orElse(null);
+    }
 }
