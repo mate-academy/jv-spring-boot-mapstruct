91998a05c391f3125a836840fee941530d8d842f
rus1980707 <rus1980707@gmail.com>
2025-02-20 17:50:02 +0200

    jv-spring-boot-mapstruct

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -3,8 +3,12 @@ package mate.academy.mapstruct.mapper;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
+@Mapper(componentModel = "spring")
 public interface GroupMapper {
+    StudentMapper INSTANCE = Mappers.getMapper(StudentMapper.class);
     GroupDto toDto(Group group);
 
     Group toModel(CreateGroupRequestDto requestDto);
