cd02150c57b746034724b85050ec2c3a85dff0d5
user <graban2253@gmail.com>
2025-03-18 01:32:13 +0200

    Finished

--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -1,9 +1,12 @@
 package mate.academy.mapstruct.mapper;
 
+import org.mapstruct.Mapper;
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
 import mate.academy.mapstruct.dto.subject.SubjectDto;
 import mate.academy.mapstruct.model.Subject;
 
+@Mapper(config = MapperConfig.class)
 public interface SubjectMapper {
     SubjectDto toDto(Subject subject);
 
