e640b565959d69473fa41f1956199a9f21ae0cea
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2025-04-11 11:51:13 +0200

    Solution - not working

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -3,9 +3,29 @@ package mate.academy.mapstruct.mapper;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.Named;
 
+import java.util.Optional;
+
+@Mapper(config = MapperConfig.class)
 public interface GroupMapper {
     GroupDto toDto(Group group);
 
     Group toModel(CreateGroupRequestDto requestDto);
+
+    @Named("groupById")
+    default Group groupById(Long id) {
+        return Optional.ofNullable(id)
+                .map(Group::new)
+                .orElse(null);
+    }
+
+    @Named("groupByObject")
+    default Long groupByObject(Group group) {
+        return Optional.ofNullable(group)
+                .map(Group::getId)
+                .orElse(null);
+    }
 }
