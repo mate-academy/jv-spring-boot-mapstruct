ea926ec3669f80b61b644567a983342aaaca0226
Geks <gekatonheyr@ukr.net>
2025-04-14 23:01:22 +0300

    Added Mapstruct annotations and methods to handle web requests.

--- src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/SubjectMapper.java
@@ -3,7 +3,10 @@ package mate.academy.mapstruct.mapper;
 import mate.academy.mapstruct.dto.subject.CreateSubjectRequestDto;
 import mate.academy.mapstruct.dto.subject.SubjectDto;
 import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValueCheckStrategy;
 
+@Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface SubjectMapper {
     SubjectDto toDto(Subject subject);
 
