d4b8b03aec505c0dc4fb0a7211c34ea570e7589d
Oleksandr-Radovets <radovietsoleksandr@gmail.com>
2025-05-20 12:00:03 +0300

    mapper2

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,10 +1,13 @@
 package mate.academy.mapstruct.mapper;
 
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import org.mapstruct.Mapper;
 
+@Mapper(config = MapperConfig.class)
 public interface StudentMapper {
     StudentDto toDto(Student student);
 
